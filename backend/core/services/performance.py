ca0e6fa932692354ac5b505aa29554b0e2e93a85
Délita Makanda <delitamakanda@users.noreply.github.com>
2025-04-24 10:44:44 +0200

    Update performance.py

--- backend/core/services/performance.py
+++ backend/core/services/performance.py
@@ -11,6 +11,6 @@ def get_current_market_price(ticker: object) -> float:
 def calculate_portfolio_value(portfolio):
     total_value = 0
     for position in portfolio.positions.all():
-        market_price = get_current_market_price(position.asset.ticker) if position.asset.ticker else float(position.price_at_buy * position.quantity)
+        market_price = get_current_market_price(position.asset.ticker) if position.asset.ticker else float(position.price_at_buy)
         total_value += float(position.quantity) * market_price
     return total_value
