76a6d8d60397d398b942dad3f6518ecd3e1d9b47
Délita Makanda <delita.makanda@gmail.com>
2025-05-10 23:55:23 +0200

    debug env vars

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='your-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
 
 
 # Application definition
