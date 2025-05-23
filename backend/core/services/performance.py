6081a97c546d03745f36eff4dd9e69a8f5906d6a
Délita Makanda <delita.makanda@gmail.com>
2025-05-23 23:52:56 +0200

    add loguru

--- backend/core/services/performance.py
+++ backend/core/services/performance.py
@@ -1,8 +1,10 @@
 import yfinance as yf
+from loguru import logger
 
 def get_current_market_price(ticker: object) -> float:
     try:
         price = yf.Ticker(ticker)
+        logger.info(f"Fetched market price for {ticker}: {price.history(period='1d')['Close'].iloc[-1]}")
         return  price.history(period="1d")['Close'].iloc[-1]
     except Exception as e:
         print(f"Error fetching market price for {ticker}: {str(e)}")
